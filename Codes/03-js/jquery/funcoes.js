5558e99760aeb1d93aee802f57714de3a614e2d2
Fernando B Oliveira <fboliveira25@gmail.com>
2017-06-01 17:20:41 -0300

    Class Jun 1st, 2017.
    JS: validation and elements manipulation; jQuery: introduction. Project
    proposal models update: LaTeX and Markdown.

---
+++ Codes/03-js/jquery/funcoes.js
@@ -0,0 +1,14 @@
+// Sintaxe:
+// $(seletor).acao()
+
+// Document.ready()
+//$(function() {
+//
+//});
+
+$(document).ready(function(){
+
+  // Todo código jQuery
+  console.log("Documento pronto para uso.");
+
+});
