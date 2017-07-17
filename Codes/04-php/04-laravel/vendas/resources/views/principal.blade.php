42259c79721a2f26dc10cadf053405b20045935b
Fernando B Oliveira <fboliveira25@gmail.com>
2017-07-17 16:10:38 -0300

    Class Jul 17th, 2017. Laravel: new project -> vendas - config and main view.

---
+++ Codes/04-php/04-laravel/vendas/resources/views/principal.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sistema de Vendas</title>
+  </head>
+  <body>
+
+    Templates: https://www.w3schools.com/bootstrap/bootstrap_templates.asp
+    Modelo - Portifólio: https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_temp_portfolio&stacked=h
+
+  </body>
+</html>
