58c41f134fcbac53941f7e114a8be6f6fa218055
Fernando B Oliveira <fboliveira25@gmail.com>
2017-12-13 09:23:33 -0200

    Class Dec 12th, 2017. JS: jQuery - introduction.

---
+++ Codes/003-js/jquery/funcoes.js
@@ -0,0 +1,38 @@
+// Sintaxe:
+// $(seletor).acao()
+
+$(document).ready(function(){
+
+    // Todo conteúdo jQuery
+    console.log("Documento pronto para uso.");
+
+    // P -> click -> ocultar
+    $("p").click(function(){
+        $(this).hide();
+    });
+
+    $("button[name='exibir']").click(function(){
+        $("#links").show();
+    });
+
+    $("button[name='ocultar']").click(function(){
+        $("#links").hide();
+    });
+
+    $("button[name='mudar']").click(function(){
+        $("#titulo").addClass("jumbotron");
+        $("button").addClass("btn btn-primary");
+        $("#links").addClass("alert alert-info");
+
+        $("p").removeClass("h2");
+
+        $("a").toggleClass("btn btn-warning");
+    });
+
+
+
+
+
+
+
+});
