f6000ab5c892f62a3bd4a027f47055cb2eda6021
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-09-28 21:14:13 -0300

    Class Sep 28, 2018. JS: introduction.

---
+++ Codes/003-js/form.js
@@ -0,0 +1,9 @@
+function calcular() {
+
+  var n1 = parseInt(document.dados.valor1.value);
+  var n2 = parseInt(document.dados.valor2.value);
+  var res = n1 + n2;
+
+  document.dados.resultado.value = res;
+
+}
