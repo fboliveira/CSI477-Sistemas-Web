7661ee48773fb9a0d3ebb31f887f4b284cbab7eb
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-11-01 07:14:55 -0300

    Class Oct 30, 2018. PHP: DB.

--- Codes/004-php/03-db/listarAlunos.php
+++ Codes/004-php/03-db/listarAlunos.php
@@ -5,12 +5,21 @@
 
 
   // Configuração
-  $database = "mysql";
+  $database = "academico";
   $dbusername = "sistemaweb";
   $dbpassword = "123456";
-  $dbhost = "localhost";
+  $dbhost = "127.0.0.1";
   $mysql = "mysql:host=$dbhost;dbname=$database";
 
   $connection = new PDO($mysql, $dbusername, $dbpassword);
 
-  var_dump($connection);
+  //var_dump($connection);
+
+  // Controller -> Model
+  $alunos = $connection->query("SELECT * FROM alunos");
+
+  //var_dump($alunos);
+
+  while ( $linha = $alunos->fetch() ) {
+    echo $linha["id"] . " - " . $linha["nome"];
+  }
