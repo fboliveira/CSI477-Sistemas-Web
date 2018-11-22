17928696bee4422be2c3a7d11a4d041c3be640fc
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-11-22 10:52:06 -0200

    Class Nov 21th, 2018. Laravel: migrations - estados.

--- Codes/004-php/04-laravel/academico/database/migrations/2018_11_21_234157_create_estados_table.php
+++ Codes/004-php/04-laravel/academico/database/migrations/2018_11_21_234157_create_estados_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateEstadosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('estados', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->tinyInteger('type')->default('0');
-            $table->rememberToken();
+            $table->string('nome');
+            $table->string('sigla', 2);
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('estados');
     }
 }
