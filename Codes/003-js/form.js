5dffcedceae6e95cb7d8c01eb908549b220b404f
Fernando B Oliveira <fboliveira25@gmail.com>
2019-05-03 15:44:03 -0300

    Class May 3rd, 2019 JS: Form: validation.

--- Codes/003-js/form.js
+++ Codes/003-js/form.js
@@ -3,12 +3,65 @@
   Comentários
 */
 
-function calcular() {
+function validar(campo, alerta, label) {
 
-  var n1 = parseFloat(document.dados.valor1.value);
+  var n = parseFloat(campo.value);
+
+  if (campo.value.length == 0 || isNaN(n)) {
+    
+    // Erro
+    document.getElementById(alerta).style.display = "block";
+
+    document.getElementById(label).classList.add("text-danger");
+
+    campo.classList.add("is-invalid");
+
+    campo.value = "";
+    campo.focus();
+    return false;
+    
+  }
+
+  // Tudo correto
+  document.getElementById(alerta).style.display = "none";
+  campo.classList.remove("is-invalid");
+  campo.classList.add("is-valid");
+
+  document.getElementById(label).classList.remove("text-danger");
+
+  return true;
+
+}
+
+function calcular() {
+ 
+  var v1 = document.dados.valor1;
+  var v2 = document.dados.valor2;
+
+  if ( validar(v1, "alerta1", "labelv1") && validar(v2, "alerta2", "labelv2") ) {
+
+    var n1 = parseFloat(v1.value);
+    var n2 = parseFloat(v2.value);
+  
+    var res = n1 + n2;
+
+    document.dados.resultado.value = res;
+      
+  }
+  
+}
+
+
+
+function calcular1() {
+
+  var v1 = document.dados.valor1;
+  var v2 = document.dados.valor2;
+
+  var n1 = parseFloat(v1.value);
   var n2 = parseFloat(document.dados.valor2.value);
 
-  if ( document.dados.valor1.value.length == 0 || isNaN(n1) ) {
+  if ( v1.value.length == 0 || isNaN(n1) ) {
 
     //window.alert("Preencha o primeiro valor corretamente!");
 
@@ -16,13 +69,22 @@ function calcular() {
 
     document.getElementById("alerta1").style.display = "block";
 
-    document.dados.valor1.value = "";
-    document.dados.valor1.focus();
+    document.getElementById("labelv1").classList.add("text-danger");
+
+    v1.classList.add("is-invalid");
+
+    v1.value = "";
+    v1.focus();
     return;
 
   }
 
   document.getElementById("alerta1").style.display = "none";
+  v1.classList.remove("is-invalid");
+  v1.classList.add("is-valid");
+
+  document.getElementById("labelv1").classList.remove("text-danger");
+  // document.getElementById("labelv1").classList.add("text-success");
 
   if ( document.dados.valor2.value.length == 0 || isNaN(n2) ) {
 
