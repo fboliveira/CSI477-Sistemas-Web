61a75083accd9c60d321aa47e798903ddeeed4eb
Fernando B Oliveira <fboliveira25@gmail.com>
2019-05-17 17:58:28 -0300

    Class May 17, 2019. PHP: introduction.

---
+++ Codes/004-php/01-introducao/index.php
@@ -0,0 +1,4 @@
+<?php
+
+  $user = "Fernando";
+  echo "Seja bem-vinda(o), $user !";
---
+++ Codes/004-php/02-login/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Acesso ao sistema</title>
+  </head>
+  <body>
+
+    <form>
+
+      <label for="user">Usuário</label>
+      <input type="text" name="usuario" value="" id="user">
+
+      <label for="password">Senha</label>
+      <input type="password" name="senha" value="" id="password">
+
+      <input type="submit" name="btnAcessar" value="Acessar">
+      <input type="reset" name="btnLimpar" value="Limpar">
+
+    </form>
+
+  </body>
+</html>
