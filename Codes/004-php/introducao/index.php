1935e0914a48f1e95d83dbc2df1227a6d16ea0b5
Fernando B Oliveira <fboliveira25@gmail.com>
2019-10-30 09:25:16 -0300

    Class Oct 29, 2019. PHP: introduction.

---
+++ Codes/004-php/introducao/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP: introdução</title>
+  </head>
+  <body>
+
+    <?php echo "Olá, mundo!"; ?>
+
+  </body>
+</html>
