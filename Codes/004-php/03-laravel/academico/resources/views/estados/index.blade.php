03f091a790f516ef4d974094ac51b53d0aeb58b5
Fernando B Oliveira <fboliveira25@gmail.com>
2019-11-18 12:09:16 -0300

    Update view: principal and estados.index.

--- Codes/004-php/03-laravel/academico/resources/views/estados/index.blade.php
+++ Codes/004-php/03-laravel/academico/resources/views/estados/index.blade.php
@@ -2,8 +2,22 @@
 
 @section('conteudo')
 
-  @foreach($estados as $e)
-    <p>{{ $e->nome }}</p>
+  <table class="table table-striped table-bordered">
+    <thead class="table-light">
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Sigla</th>
+      </tr>
+    </thead>
+    <tbody>
+  @foreach ($estados as $e)
+      <tr>
+        <td>{{ $e->id }}</td>
+        <td>{{ $e->nome }}</td>
+        <td>{{ $e->sigla }}</td>
+      </tr>
   @endforeach
-
+    </tbody>
+  </table>
 @endsection
--- Codes/004-php/03-laravel/academico/resources/views/principal.blade.php
+++ Codes/004-php/03-laravel/academico/resources/views/principal.blade.php
@@ -7,15 +7,24 @@
   <body>
 
     <!-- Links - menu lateral //-->
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/welcome">Sobre</a></li>
-        <li><a href="/listar">Listar pessoas</a></li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #563D7C;">
+
+      <span class="navbar-brand mb-0 h1">Acadêmico</span>
+
+      <ul class="navbar-nav">
+          <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="/welcome" class="nav-link">Sobre</a></li>
+          <li class="nav-item"><a href="/listar" class="nav-link">Listar pessoas</a></li>
+      </ul>
+    </nav>
 
     <!-- Conteúdo -- parte central //-->
     @yield('conteudo')
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
