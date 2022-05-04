b3e0a0e0268711ab60e9903f655ed4d885a58c5f
Fernando B Oliveira <fboliveira25@gmail.com>
2022-05-04 14:21:48 -0300

    feat(PHP): :sparkles: Introduction

---
+++ Codes/005-php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Introdução</title>
+</head>
+<body>
+    <h1>Introdução ao PHP</h1>
+
+    <?php $name = "Fernando" ?>
+
+    <h2>Olá, <?php echo $name ?></h2>
+
+
+</body>
+</html>
