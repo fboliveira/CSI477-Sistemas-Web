e62362b950edd13b72de24b1592caa9485ec9e53
Fernando B Oliveira <fboliveira25@gmail.com>
2022-05-11 19:31:03 -0300

    feat(Laravel): :sparkles: Add Estado scaffold

---
+++ Codes/006-laravel/sistema/app/Http/Controllers/EstadoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEstadoRequest;
+use App\Http\Requests\UpdateEstadoRequest;
+use App\Models\Estado;
+
+class EstadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreEstadoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreEstadoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Estado  $estado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Estado $estado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Estado  $estado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Estado $estado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateEstadoRequest  $request
+     * @param  \App\Models\Estado  $estado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateEstadoRequest $request, Estado $estado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Estado  $estado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Estado $estado)
+    {
+        //
+    }
+}
