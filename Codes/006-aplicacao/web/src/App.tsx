df82c984ac7829bfe70b654a7e9930d4ccbb35e6
Fernando B Oliveira <fboliveira25@gmail.com>
2022-09-16 17:24:33 -0300

    feat(React.js): :sparkles: Add list estados: state, effects and routes

--- Codes/006-aplicacao/web/src/App.tsx
+++ Codes/006-aplicacao/web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ListEstados from './components/estados/ListEstados';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header name="fboliveira" />
+      <ListEstados />
     </div>
   );
 }
--- Codes/006-aplicacao/web/src/index.tsx
+++ Codes/006-aplicacao/web/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AppRoutes from './routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AppRoutes />
   </React.StrictMode>
 );
