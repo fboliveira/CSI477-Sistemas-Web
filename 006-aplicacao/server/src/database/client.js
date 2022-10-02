fb7037d64cd0b506ecf6507839ab6f479be5157e
Ayron Luigi de paiva <49703457+ImAyron@users.noreply.github.com>
2022-10-01 22:36:46 -0300

    .

--- 006-aplicacao/server/src/database/client.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// https://www.prisma.io/docs/concepts/components/prisma-client/working-with-prismaclient/logging
-const prismaClient = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-  });
-
-export { prismaClient }
