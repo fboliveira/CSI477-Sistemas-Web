5b8bf1a2ffce91fc486706f58e1c92c921bc720d
Fernando B Oliveira <fboliveira25@gmail.com>
2023-02-16 20:50:05 -0300

    Node.js: introduction

--- Codes/006-nodejs/introducao/src/server.js
+++ Codes/006-nodejs/introducao/src/server.js
@@ -1,29 +1,10 @@
 // Import
-import os from 'os';
-import express from 'express';
-
+import app, { config } from './routes.js';
 
 // Rotas -- express.js
 
-// Iniciar o servidor:
-console.log('Hello world!');
-console.log(`Servername: ${os.hostname()}`);
-
-const freememory = os.freemem() / (1024 * 1024 * 1024);
-const total = os.totalmem() / (1024 * 1024 * 1024);
-
-console.log(`Free memory: ${freememory} GB`);
-console.log(`Total: ${total} GB`);
-
 // API Server ----
-const server = express();
-
-// Routes
-server.get('/', ( request, response ) => {
-    response.send('Página inicial da aplicação');
-});
-
-server.listen(4000, () => {
-    console.log(`Server is running...`);
+ app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}...`);
 });
 
