a5e1c321a302f7be4fc01e9a4bbb4fb7f1d30c9d
Fernando B Oliveira <fboliveira25@gmail.com>
2023-03-07 20:37:04 -0300

    React/TypeScript - intro: CRA and Vite

---
+++ Codes/007-aplicacao/web/web-vite/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Menu from './components/menu/Menu'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <Menu />
+    </div>
+  )
+}
+
+export default App
---
+++ Codes/007-aplicacao/web/web-vite/src/components/menu/Menu.tsx
@@ -0,0 +1,16 @@
+import './menu.css'
+
+const Menu = () => {
+
+    return(
+        <div>
+            <h2>Menu</h2>
+            <div>
+                <p>Opções</p>
+            </div>
+        </div>
+    );
+
+}
+
+export default Menu;
