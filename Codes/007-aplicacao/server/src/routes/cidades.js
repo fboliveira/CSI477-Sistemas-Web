ddfe775ec6d263f84057935092c05327430a322a
Fernando B Oliveira <fboliveira25@gmail.com>
2023-03-13 16:03:52 -0300

    Add CRUD cidades - update

--- Codes/007-aplicacao/server/src/routes/cidades.js
+++ Codes/007-aplicacao/server/src/routes/cidades.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { CreateCidadeController } from '../controller/cidades/CreateCidadeController.js';
 import { GetAllCidadeController } from '../controller/cidades/GetAllCidadeController.js';
-import { GetByIdCidadeController } from '../controller/cidades/GetByIdCidadeController.js'
-
+import { GetByIdCidadeController } from '../controller/cidades/GetByIdCidadeController.js';
+import { UpdateCidadeController } from '../controller/cidades/UpdateCidadeController.js';
+ 
 const cidadeRouter = Router();
 
 // Create
@@ -18,6 +19,8 @@ const getByIdCidadeController = new GetByIdCidadeController();
 cidadeRouter.get('/cidades/:id', getByIdCidadeController.handle);
 
 // Update
+const updateCidadeController = new UpdateCidadeController();
+cidadeRouter.put('/cidades', updateCidadeController.handle);
 
 // Delete
 
