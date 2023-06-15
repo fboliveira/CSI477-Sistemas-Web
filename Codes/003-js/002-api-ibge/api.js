053efb6bf127e231f7c97e947152dc16306c2265
Fernando B Oliveira <fboliveira25@gmail.com>
2023-06-15 17:42:37 -0300

    JS: API - final version

--- Codes/003-js/002-api-ibge/api.js
+++ Codes/003-js/002-api-ibge/api.js
@@ -21,17 +21,87 @@ async function carregarEstados() {
 
 }
 
+async function carregarDadosCidades() {
+
+    // Qual é o estado selecionado
+    const selectEstados = document.getElementById("estados");
+
+    const estado_index = selectEstados.selectedIndex;
+
+    limparSelect("cidades");
+
+    const estado_id = selectEstados.options[estado_index].value;
+
+    if (estado_index === 0 || estado_id === "") {
+        return;
+    }
+
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado_id}/municipios`);
+
+    const cidades = await response.json();
+    preencherSelectCidades(cidades);
+
+}
+
 function preencherSelectEstados(estados) {
 
     // hydration
     
-    for(let i in estados) {
-        // console.log(estados[i]);
-        console.log(i);
-    }
+    // for(let i in estados) {
+    //     // console.log(estados[i]);
+    //     console.log(i);
+    // }
+
+    const select = document.getElementById("estados");
+
+    limparSelect("estados");
+
+    estados.sort( (a, b) => a.nome.localeCompare(b.nome) );
 
     for(let estado of estados) {
-        console.log(estado);
+        // const id = estado.id;
+        // const nome = estado.nome;
+        // const sigla = estado.sigla;
+
+        // Destructing
+        const { id, nome, sigla } = estado;
+        // const [ ] = estado;
+
+        const option = document.createElement("option");
+
+        option.value = id;
+        // Template literals
+        option.innerHTML = `${nome}-${sigla}`;
+
+        select.appendChild(option);
+ 
+    }
+
+}
+
+function preencherSelectCidades(cidades) {
+
+    const selectCidades = document.getElementById("cidades");
+
+    for(const cidade of cidades) {
+
+        const { id, nome } = cidade;
+
+        const option = document.createElement("option");
+
+        option.value = id;
+        option.innerHTML = nome;
+
+        selectCidades.appendChild(option);
+
+    }
+}
+
+function limparSelect(idDoCampo) {
+    const select = document.getElementById(idDoCampo);
+
+    while(select.length > 1) {
+        select.remove(1);
     }
 
 }
