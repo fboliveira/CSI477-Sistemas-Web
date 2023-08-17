f5dca9c2607c332614637a9616beb30e04263211
Fernando B Oliveira <fboliveira25@gmail.com>
2023-08-17 17:17:48 -0300

    Update component - update and delete functions

--- Codes/005-aplicacao/web-nextjs/app/components/CidadeTable.tsx
+++ Codes/005-aplicacao/web-nextjs/app/components/CidadeTable.tsx
@@ -2,12 +2,15 @@
 
 import Link from "next/link";
 import CidadeDTO from "../types/CidadeDTO";
-import getAllCidades from "../repository/cidades/GetAllCidades";
 import { useEffect, useState } from "react";
 
-export default function CidadeTable() {
+const getAllCidades = async () => {
+    const cidades = await fetch('http://localhost:5555/cidades', { cache: 'no-store' });
+    return cidades.json();
+}
 
-    const [ cidades, setCidades ] = useState<CidadeDTO[]>([])
+
+export default function CidadeTable() {
 
     const format = (dateString : string | undefined) => {
         
@@ -21,14 +24,53 @@ export default function CidadeTable() {
 
     }
 
+    const [ cidades, setCidades ] = useState<CidadeDTO[]>([]);
+
     // const cidades : CidadeDTO[] = await getAllCidades();
 
     useEffect(() => {
         getAllCidades()
             .then((data) => {
-                setCidades(data)
-            });
-    }, [])
+                setCidades(data);
+            })
+    }, []);
+
+    const handleDelete = async (id : number | undefined) => {
+    
+        const data = {
+          id,
+        };
+    
+        const requestInit: RequestInit = {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        };
+    
+        try {
+          const response = await fetch(
+            "http://localhost:5555/cidades",
+            requestInit
+          );
+    
+          if (response.ok) {
+            const cidade = await response.json();
+            const { id } = cidade;
+            window.alert(`Cidade excluída com sucesso! Id: ${id}`);
+            
+            // Atualizar a lista
+            setCidades(cidades.filter(
+                elem => elem.id != cidade.id
+            ))  
+
+          }
+        } catch (error) {
+          window.alert("Erro na exclusão da Cidade!");
+          console.error(error);
+        }
+      };    
 
     return(
         <table>
@@ -39,15 +81,16 @@ export default function CidadeTable() {
                     <th>Estado</th>
                     <th>Criado em</th>
                     <th>Última atualização</th>
-                    <th>Editar</th>
+                    <th>Alterar</th>
                     <th>Excluir</th>
+                    <th>Excluir - handle</th>
                 </tr>
             </thead>
 
             <tbody>
 
                 {
-                    cidades.map(( cidade : CidadeDTO ) => {
+                    cidades.map(( cidade ) => {
 
                         return(
                             <tr key={cidade.id}>
@@ -56,8 +99,24 @@ export default function CidadeTable() {
                                 <td>{cidade.estado?.nome}-{cidade.estado?.sigla}</td>
                                 <td>{format(cidade.created_at)}</td>
                                 <td>{format(cidade.updated_at)}</td>
-                                <td><Link href={`/cidades/update/${cidade.id}`}>Atualizar</Link></td>
-                                <td><Link href={`/cidades/delete/${cidade.id}`}>Excluir</Link></td>
+                                <td><Link 
+                                    href={`/cidades/update/${cidade.id}`}>
+                                        Atualizar
+                                    </Link></td>
+                                <td><Link 
+                                    href={`/cidades/delete/${cidade.id}`}>
+                                        Excluir
+                                    </Link>
+                                </td>
+                                <td>
+                                    <button 
+                                        onClick={() => {
+                                            if (window.confirm("Confirma exclusão?")) {
+                                                handleDelete(cidade.id)
+                                            }
+                                        }}
+                                        >Excluir - handle</button>
+                                </td>                        
                             </tr>
                         )
 
