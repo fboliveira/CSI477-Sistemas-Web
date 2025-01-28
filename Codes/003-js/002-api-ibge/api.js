da042d867366708dae5a23274b56fe61770dac49
Fernando B Oliveira <fboliveira25@gmail.com>
2025-01-27 22:00:39 -0300

    Update JS: API IBGE: estados

--- Codes/003-js/002-api-ibge/api.js
+++ Codes/003-js/002-api-ibge/api.js
@@ -1,1 +1,45 @@
 // https://servicodados.ibge.gov.br/api/v1/localidades/estados
+
+const URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+
+async function carregarEstados() {
+
+    const response = await fetch(URL)
+    console.log(response)
+    const estados = await response.json()
+    console.log(estados)
+
+    preencherSelectEstados(estados)
+
+}
+
+function preencherSelectEstados(estados) {
+
+    // for(const i in estados) {
+    //     console.log(estados[i].nome)
+    // }
+
+    const select = document.getElementById("estados")
+
+    for(const estado of estados) {
+        // console.log(estado.nome)
+
+        // const id = estado.id;
+        // const nome = estado.nome;
+        // const sigla = estado.sigla;
+
+        // Destructing
+        const {id, nome, sigla} = estado;
+
+        const option = document.createElement("option");
+        option.value = id;
+        option.innerHTML = nome;
+
+        select.appendChild(option);
+
+        // hydration
+
+    }
+
+
+}
