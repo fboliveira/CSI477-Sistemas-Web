ae173d6e40912c833072e0ed1bc1bb46beb8e4d0
Fernando B Oliveira <fboliveira25@gmail.com>
2025-01-30 08:09:29 -0300

    Updated JS: IBGE API: cities

--- Codes/003-js/002-api-ibge/api.js
+++ Codes/003-js/002-api-ibge/api.js
@@ -20,6 +20,9 @@ function preencherSelectEstados(estados) {
     // }
 
     const select = document.getElementById("estados")
+    limparSelect("estados")
+
+    estados.sort( (a, b) => a.nome.localeCompare(b.nome))
 
     for(const estado of estados) {
         // console.log(estado.nome)
@@ -33,7 +36,9 @@ function preencherSelectEstados(estados) {
 
         const option = document.createElement("option");
         option.value = id;
-        option.innerHTML = nome;
+        // option.innerHTML = nome + '-' + sigla;
+        // Template literals
+        option.innerHTML = `${nome}-${sigla}`;
 
         select.appendChild(option);
 
@@ -41,5 +46,80 @@ function preencherSelectEstados(estados) {
 
     }
 
+}
+
+function limparSelect(idDoCampo) {
+
+    const select = document.getElementById(idDoCampo)
+
+    while(select.length > 1) {
+        select.remove(1)
+    }
+}
+
+function carregarCidades() {
+
+    limparSelect("cidades")
+
+    // Qual é o estado que está selecionado:
+    // -- Recuperar o select de estados
+    const selectEstados = document.getElementById("estados")
+
+    // Índice do option selecionado:
+    const index = selectEstados.selectedIndex
+
+    // ID do estado: value do option selecioado - index
+    const id = selectEstados.options[index].value
+
+    if (index === 0 || id === "") {
+        return
+    }
+
+    // URL/id/municipios
+    const url = `${URL}/${id}/municipios`
+
+    fetch(url)
+        .then( dados => dados.json() )
+        .then (cidades => preencherSelectCidades(cidades))
 
 }
+
+const preencherSelectCidades = (cidades) => {
+
+    const selectCidades = document.getElementById("cidades")
+
+    for(const cidade of cidades) {
+
+        const { id, nome } = cidade;
+
+        const option = document.createElement("option")
+
+        option.value = id;
+        option.innerHTML = nome
+
+        selectCidades.appendChild(option)
+    }
+
+}
+
+
+
+
+
+
+
+
+// Classica
+function soma (a, b) {
+    return a + b
+}
+
+// Arrow function
+const somaArrow = (a, b) => {
+    return a + b;
+}
+
+const somaArrowInLine = (a, b) => a + b;
+
+soma(4, 5)
+somaArrow(2, 3)
