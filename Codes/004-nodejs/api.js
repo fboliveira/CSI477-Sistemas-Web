7ae8962b4a9b67e5e3895473b8fd928ac1c698c1
Fernando B Oliveira <fboliveira25@gmail.com>
2025-02-05 22:03:23 -0300

    Added Nodejs: first class

---
+++ Codes/004-nodejs/api.js
@@ -0,0 +1,15 @@
+export default function start() {
+    console.log('Server is runing...');
+}
+
+export function stop() {
+    console.log("Server stopped.")
+}
+
+export function log() {
+
+}
+
+// module.exports = { start }
+// export default start
+// export { stop, log }
