2b4f4a6dda380192fe2d1bfbf523a88bf940df19
Fernando B Oliveira <fboliveira25@gmail.com>
2025-02-20 09:37:23 -0300

    Updated get by id and create

--- Codes/005-aplicacao/server/src/routes/projects.js
+++ Codes/005-aplicacao/server/src/routes/projects.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 
 import { GetAllProjectController } from "../controller/projects/GetAllProjectController.js";
+import { GetByIdProjectController } from "../controller/projects/GetByIdProjectController.js";
+
+import { CreateProjectController } from "../controller/projects/CreateProjectController.js";
 
 const projectsRouter = Router()
 
@@ -9,8 +12,12 @@ const getAllProjectController = new GetAllProjectController();
 projectsRouter.get('/projects', getAllProjectController.handle)
 
 // Get by id - R
+const getByIdProjectController = new GetByIdProjectController()
+projectsRouter.get('/projects/:id', getByIdProjectController.handle)
 
 // Create - C
+const createProjectController = new CreateProjectController()
+projectsRouter.post('/projects', createProjectController.handle)
 
 // Update: PUT/PATCH - U
 
