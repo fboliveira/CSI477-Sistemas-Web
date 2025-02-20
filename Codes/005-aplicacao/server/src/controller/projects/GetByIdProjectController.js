b79e42ef975381eb2377b92cba371848fb501a45
Fernando B Oliveira <fboliveira25@gmail.com>
2025-02-20 09:37:05 -0300

    Updated try/catch

--- Codes/005-aplicacao/server/src/controller/projects/GetByIdProjectController.js
+++ Codes/005-aplicacao/server/src/controller/projects/GetByIdProjectController.js
@@ -1,17 +1,33 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library"
 import { prisma } from "../../database/clients.js"
 
-export class GetByIdEstadoController {
+export class GetByIdProjectController {
 
     async handle(request, response) {
 
-        const { id } = request.params
-        const project = await prisma.project.findFirstOrThrow({
-            where: {
-                id: parseInt(id)
+        // request.body
+        // params -> /projects/2 > /projects/{id}
+        // query -> /projects?id=2&nome=web
+
+        try {
+            const { id } = request.params
+            const project = await prisma.project
+            .findFirstOrThrow({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+            
+            return response.json(project)
+        } catch(error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                return response.status(404).json({
+                    message: "Invalid ID.",
+                    error
+                })
             }
-        })
-        
-        return response.json(project)
+            return response.status(500).json(error)
+        }
 
     }
 
