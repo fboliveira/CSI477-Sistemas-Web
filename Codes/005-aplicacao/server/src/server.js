dc358d367877882d87d716167b85ca3ee2823eff
Fernando B Oliveira <fboliveira25@gmail.com>
2025-02-26 22:03:40 -0300

    Added create tasks

--- Codes/005-aplicacao/server/src/server.js
+++ Codes/005-aplicacao/server/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { mainRouter } from './routes/main.js'
 import { projectsRouter } from './routes/projects.js'
+import { taskRouter } from './routes/tasks.js'
 
 const server = express()
 const PORT = 5550
@@ -10,6 +11,7 @@ server.use(express.json())
 // Routes
 server.use(mainRouter)
 server.use(projectsRouter)
+server.use(taskRouter)
 
 
 // Start - listen:
