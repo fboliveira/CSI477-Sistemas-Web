533e7e190ed198f74088c8816e849a9eeb33b85d
Fernando B Oliveira <fboliveira25@gmail.com>
2025-05-13 13:14:35 -0300

    updated: table

--- Codes/005-aplicacao/web/web-nextjs/app/projects/page.tsx
+++ Codes/005-aplicacao/web/web-nextjs/app/projects/page.tsx
@@ -14,22 +14,33 @@ export default async function Projects() {
             <Link href="/projects/create">Cadastrar</Link>
 
             <ul>
+        <table className="table-auto w-full border border-collapse border-stone-950">
+                    <thead>
+                        <tr className="bg-amber-400 font-bold p-2">
+                            <th>Id</th>
+                            <th>Nome</th>
+                            <th>Atualizar</th>
+                            <th>Excluir</th>
+                        </tr>
+                    </thead>
+                    <tbody>
                 {
                     projects.map((project) =>{
                         return (
 
-                            <li key={project.id}>
-                                <Link href={`/projects/update/${project.id}`}>
-                                    {project.id} - {project.name}
-                                </Link><span> </span> 
-                                <Link href={`/projects/delete/${project.id}`}>
-                                    Excluir
-                                </Link> 
-                            </li>
+                            <tr key={project.id} className="border border-stone-400">
+
+                                <td className="justify-center">{project.id}</td>
+                                <td>{project.name}</td>
+                                <td><Link href={`/projects/update/${project.id}`}>Atualizar</Link></td>
+                                <td><Link href={`/projects/delete/${project.id}`}>Excluir</Link></td> 
+                            </tr>
                         )
                     })
                     
                 }
+                </tbody>
+                </table>
             </ul>
 
         </div>
