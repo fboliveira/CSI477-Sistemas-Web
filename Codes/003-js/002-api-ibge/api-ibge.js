b2f268e110f8fa7b9ad69319d2a09e02fdbe63c2
Fernando B Oliveira <fboliveira25@gmail.com>
2025-07-01 15:05:53 -0300

    feat(Javascript): :sparkles: Carregamento de estados e início das cidades

--- Codes/003-js/002-api-ibge/api-ibge.js
+++ Codes/003-js/002-api-ibge/api-ibge.js
@@ -32,13 +32,101 @@ async function carregarEstados() {
 
     // Percorrer o array retornado pela API
     // for-in (C: for(int i = 0; i < tam; i++))
-    for(const i in estados) {
-        console.log(estados[i].nome)
-    }
+    // for(const i in estados) {
+    //     console.log(estados[i].nome)
+    // }
+
+    // Ordenar os estados pelo nome - ASC
+    // Array.sort()
+    // Arrow function
+    estados.sort(
+        (a, b) => a.nome.localeCompare(b.nome)
+    )
+
+    limparSelect("estados")
 
     // for-of (Java: for-each -> for(item : Lista))
     for(const estado of estados) {
-        console.log(estado.sigla)
+        
+        // const id = estado.id;
+        // const nome = estado.nome;
+        // const sigla = estado.sigla;
+
+        // const regiao_id = estado.regiao.id;
+
+        // Destructing 
+        // JSON
+        const { id, nome, sigla } = estado;
+        // Array -> destructing
+        // pessoa = [1, 'Fernando']
+        // const { id, nome } = pessoa
+
+        const option = document.createElement("option");
+
+        option.value = id;
+        // option.innerHTML = nome + '-' + sigla;
+        // Template literals
+        option.innerHTML = `${nome}-${sigla} (${id})` ;
+
+        selectEstados.appendChild(option);
+
     }
 
 }
+
+function limparSelect(selectId) {
+
+    const campoSelect = document.getElementById(selectId)
+
+    // Remover options do select, com exceção do "Selecione:"
+    // Selecione -> option de index 0;
+
+    while(campoSelect.length > 1) {
+        campoSelect.remove(1) // index
+    }
+
+}
+
+// Cidades:
+async function carregarCidades() {
+
+    limparSelect("cidades")
+
+    // Qual é o estado (UF) que está selecionado? ID do Estado
+    // -- recuperar o select de estados
+    const selectEstados = document.getElementById("estados")
+    // -- índice do estado selecionado
+    const estado_index = selectEstados.selectedIndex;
+    // -- id (value) do estado selecionado
+    const estadoId = selectEstados.options[estado_index].value;
+
+    window.alert(estadoId)
+
+}
+
+
+
+
+/// Funções em JS
+
+// Funcao: Recebe dois parâmetros e retorna a soma.
+
+// Clássica/canônica
+function soma(a, b) {
+    return a + b;
+}
+
+soma(5, 6)
+
+// Arrow functions: (parâmetros) => { 
+//      corpo 
+// }
+const somaArrow = (a, b) => {
+    return a + b;
+}
+
+somaArrow(5, 6)
+
+// Arrow function - inline (return direto)
+const somaArrowInLine = (a, b) => a + b;
+somaArrowInLine(5, 6)
