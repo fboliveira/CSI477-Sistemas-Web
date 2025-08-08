e5408ea51086086b8d5a003db8baeffdda23dfbe
Fernando B Oliveira <fboliveira25@gmail.com>
2025-08-08 17:11:50 -0300

    added: menu component

---
+++ Codes/005-project-control-app/web/src/components/menu/Menu.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import "./menu.css"
+
+const Menu = () => {
+
+    return(
+        <div>
+
+            <h2>Menu</h2>
+
+            <ul>
+                <li> <Link to="/">Home</Link> </li>
+                <li> <Link to="/projects">Projetos</Link> </li>
+            </ul>
+
+        </div>
+    )
+
+}
+
+export default Menu
