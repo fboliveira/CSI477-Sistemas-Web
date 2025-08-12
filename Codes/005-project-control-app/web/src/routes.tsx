4131558ea5a2f377b9e7c50a5f7b25a9eb2b718a
Fernando B Oliveira <fboliveira25@gmail.com>
2025-08-12 16:26:18 -0300

    updated: routes and data

--- Codes/005-project-control-app/web/src/routes.tsx
+++ Codes/005-project-control-app/web/src/routes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import App from "./App"
 import ListProjects from "./pages/projects/ListProjects"
+import CreateProject from "./pages/projects/CreateProjects"
+import UpdateProject from "./pages/projects/UpdateProjects"
 
 const AppRoutes = () => {
 
@@ -12,6 +14,15 @@ const AppRoutes = () => {
 
                 <Route path="/" element={<App />} />
                 <Route path="/projects" element={<ListProjects />} />
+                <Route 
+                    path="/projects/create" 
+                    element={<CreateProject />} 
+                />
+
+                <Route 
+                    path="/projects/:id"
+                    element={<UpdateProject />}
+                />
 
             </Routes>
         
