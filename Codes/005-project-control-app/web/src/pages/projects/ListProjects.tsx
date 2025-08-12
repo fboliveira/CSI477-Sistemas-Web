f665c44e73016b07875299f3ea37cc66c3b62eca
Fernando B Oliveira <fboliveira25@gmail.com>
2025-08-12 16:26:06 -0300

    updated: change presentation to table

--- Codes/005-project-control-app/web/src/pages/projects/ListProjects.tsx
+++ Codes/005-project-control-app/web/src/pages/projects/ListProjects.tsx
@@ -23,19 +23,37 @@ const ListProjects = () => {
         <>
             <h2>Lista de projetos</h2>
             <div>
-                <Link to="/">Voltar</Link>
                 <Menu />
+                <Link to="/projects/create">Cadastrar</Link>
             </div>
 
-            <ul>
-                {
-                    projects.map( p => (
-                        <li key={p.id}>{p.name}</li>
-                      )
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Criado</th>
+                        <th>Alterado</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        projects.map( p => (
+                            <tr key={p.id}>
+                                <td>{p.id}</td>
+                                <td>{p.name}</td>
+                                <td>{p.created_at}</td>
+                                <td>{p.updated_at}</td>
+                                <td><Link to={`/projects/${p.id}`}>Atualizar</Link></td>
+                            </tr>
+                            )
 
-                    )
-                }
-            </ul>
+                        )
+                    }
+                </tbody>
+            </table>
+
 
         </>
     )
