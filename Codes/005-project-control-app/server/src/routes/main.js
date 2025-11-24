c1515a505b6face8dd4afadf0a319a2b7f1a847b
Fernando B Oliveira <fboliveira25@gmail.com>
2025-11-24 20:05:49 -0300

    updated: status route

--- Codes/005-project-control-app/server/src/routes/main.js
+++ Codes/005-project-control-app/server/src/routes/main.js
@@ -11,4 +11,11 @@ mainRouter.get('/', (request, response) => {
     response.send("<h1>Project Control Server</h1>")
 })
 
+mainRouter.get('/status', (request, response) => {
+    response.json({
+        code: 200,
+        message: "Project Control API Server is running."
+    })
+})
+
 export { mainRouter }
