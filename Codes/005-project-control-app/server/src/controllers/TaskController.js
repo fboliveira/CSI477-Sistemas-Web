737a602b27fbb1d2b713cccc40304e0b045d2f5b
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-15 19:50:14 -0300

    added: tasks controller

---
+++ Codes/005-project-control-app/server/src/controllers/TaskController.js
@@ -0,0 +1,39 @@
+// CRUD - tasks
+// C: create
+// R: read - get all / get by id /  get by name
+// U: update
+// D: delete
+
+import { prisma } from "../repository/client.js";
+
+export default class TaskController {
+
+    async getAll(request, response) {
+
+    }
+
+    async getById(request, response) {
+        
+    }
+
+    async getByName(request, response) {
+        
+    }
+
+    async getByProject(request, response) {
+        
+    }
+
+    async create(request, response) {
+        
+    }
+
+    async update(request, response) {
+        
+    }
+
+    async delete(request, response) {
+        
+    }
+
+}
