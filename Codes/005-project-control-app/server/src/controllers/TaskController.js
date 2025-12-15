89b1ee0db4ad6d201ac0fce3217c0435afbdd16b
Fernando B Oliveira <fboliveira25@gmail.com>
2025-12-15 20:21:01 -0300

    updated: get all and create

--- Codes/005-project-control-app/server/src/controllers/TaskController.js
+++ Codes/005-project-control-app/server/src/controllers/TaskController.js
@@ -10,6 +10,20 @@ export default class TaskController {
 
     async getAll(request, response) {
 
+        const tasks = await prisma.task.findMany({
+            select: {
+                id: true,
+                description: true,
+                project: {
+                    select: {
+                        id: true,
+                        name: true
+                    }
+                }
+            }
+        })
+        return response.json(tasks)
+
     }
 
     async getById(request, response) {
@@ -25,6 +39,36 @@ export default class TaskController {
     }
 
     async create(request, response) {
+
+        const { description, project_id } = request.body
+
+        // Is project_id valid?
+        // Model -> validate()
+
+        try {
+            
+            const task = await prisma.task.create({
+                data: {
+                    description,
+                    project: {
+                        connect: {
+                            id: project_id
+                        }
+                    }
+                }
+            })
+
+            return response.json(task)
+
+        } catch (error) {
+
+            return response.status(400).json({
+                code: 400,
+                message: "Invalid request.",
+                error
+            })
+            
+        }
         
     }
 
