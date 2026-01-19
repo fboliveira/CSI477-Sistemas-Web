dff31b1d1172e2c6ff73d1e3c54b5096f455a2df
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-19 20:34:38 -0300

    added: Menu component

---
+++ Codes/005-project-control-app/web/src/components/Menu.tsx
@@ -0,0 +1,23 @@
+const Menu = () => {
+
+    return(
+
+        <div>
+
+            <h2>Opções</h2>
+
+            <ul>
+                <li>Home</li>
+                <li>Projetos</li>
+                <li>Tarefas</li>
+                <li>Relatórios</li>
+                <li>Perfil</li>
+            </ul>
+
+        </div>
+
+    )
+
+}
+
+export default Menu
