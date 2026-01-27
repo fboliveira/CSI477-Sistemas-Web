6fcbe89b4272f047892faa9ee6a8e81b1cd40328
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-27 16:20:40 -0300

    updated: Link

--- Codes/005-project-control-app/web/src/components/Menu.tsx
+++ Codes/005-project-control-app/web/src/components/Menu.tsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom"
+
+import "./menu.css"
+
 const Menu = () => {
 
     return(
 
         <div>
 
-            <h2>Opções</h2>
+            <h2 className="text-2xl font-semibold">Opções</h2>
 
             <ul>
-                <li>Home</li>
-                <li>Projetos</li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/projects">Projetos</Link></li>
                 <li>Tarefas</li>
                 <li>Relatórios</li>
                 <li>Perfil</li>
