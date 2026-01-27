435836acdff10f317477315041c2166d016e3be9
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-26 21:18:31 -0300

    added: application header

---
+++ Codes/005-project-control-app/web/src/components/AppHeader.tsx
@@ -0,0 +1,16 @@
+import Menu from "./Menu"
+
+
+
+const AppHeader = () => {
+
+    return(
+        <>
+            <h1 className="text-3xl font-bold">Controle de Projetos</h1>
+            <Menu />
+        </>
+    )
+
+}
+
+export default AppHeader
