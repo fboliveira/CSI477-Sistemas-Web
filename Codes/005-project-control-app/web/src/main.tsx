248387d0c33579895aae1c95e38947b199e86b13
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-26 21:29:05 -0300

    updated: app routes

--- Codes/005-project-control-app/web/src/main.tsx
+++ Codes/005-project-control-app/web/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import AppRoutes from './routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>,
 )
