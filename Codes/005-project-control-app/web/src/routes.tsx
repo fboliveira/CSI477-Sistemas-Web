479ec54ceb37fa2a2959bae2b6f47cc1f0d2550f
Fernando B Oliveira <fboliveira25@gmail.com>
2026-01-29 10:43:53 -0300

    updated: create project route

--- Codes/005-project-control-app/web/src/routes.tsx
+++ Codes/005-project-control-app/web/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import ListProjects from "./pages/projects/ListProjects";
+import CreateProject from "./pages/projects/CreateProject";
 
 export default function AppRoutes() {
 
@@ -18,6 +19,11 @@ export default function AppRoutes() {
                 <Route 
                     path="/projects"
                     element={<ListProjects />}
+                />
+
+                <Route
+                    path="/projects/create"
+                    element={<CreateProject />}
                 />
                 
             </Routes>
