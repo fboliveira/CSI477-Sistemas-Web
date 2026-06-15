1d61a4529ac689039c8429c823e6baa65c70882c
Fernando B Oliveira <fboliveira25@gmail.com>
2026-06-15 10:30:01 -0300

    feat: added get by id

--- Codes/005-project-control-app/server/src/controller/ProjectController.js
+++ Codes/005-project-control-app/server/src/controller/ProjectController.js
@@ -12,16 +12,44 @@ export default class ProjectController {
     // R: read
     async getAll (request, response) {
 
+        // Client -> Model : Service
         const projects = await prisma.project.findMany()
         return response.json(projects)
 
     }
 
-    getById(request, response) {
+    // R: id
+    // GET
+    // /api/projects?id=1 -> query: request.query
+    // /api/projects/1 -> params: request.params
+    async getById(request, response) {
+
+        // GET: /api/projects/1 -> /api/projects/{id} : params
+        try {
+            const { id } = request.params
+
+            const project = await prisma.project.findFirstOrThrow({
+                where: {
+                    id: parseInt(id)
+                }
+            })
+
+            return response.json(project)
+
+        } catch(error) {
+            return response
+                .status(400) // Bad request
+                .json({
+                    message: "Invalid id.",
+                    error
+                })
+        }
 
     }
 
     getByName(request, response) {
+
+        // GET: /api/projects/net
 
     }
     // -- 
