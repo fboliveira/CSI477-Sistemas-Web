2fd66a52b96f59c2573a4a88c761ec2ab25a9e27
Fernando B Oliveira <fboliveira25@gmail.com>
2026-06-17 16:10:53 -0300

    feat prisma client

--- Codes/005-project-control-app/server/src/repository/client.js
+++ Codes/005-project-control-app/server/src/repository/client.js
@@ -1,5 +1,13 @@
-import { PrismaClient } from "../generated/prisma/client"
+// 
+// npm install @prisma/adapter-better-sqlite3 better-sqlite3
 
-const prisma = new PrismaClient()
+import { PrismaClient } from "../generated/prisma/client.js"
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+const adapter = new PrismaBetterSqlite3({
+  url: process.env.DATABASE_URL ?? 'file:./aplicacao.sqlite',
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export { prisma }
