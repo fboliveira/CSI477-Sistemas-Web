71b9f7ed84ebd57eba46136f90916b7d6f024498
Fernando B Oliveira <fboliveira25@gmail.com>
2026-06-18 10:01:47 -0300

    fix: prisma client

--- Codes/005-project-control-app/server/src/repository/client.js
+++ Codes/005-project-control-app/server/src/repository/client.js
@@ -1,5 +1,12 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../generated/prisma/client.js"
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3"
 
-const prisma = new PrismaClient()
+const adapter = new PrismaBetterSqlite3({
+    url: "file:./project-control.db"
+})
+
+const prisma = new PrismaClient({
+    adapter
+})
 
 export { prisma }
