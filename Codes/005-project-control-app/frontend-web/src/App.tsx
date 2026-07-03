8879ace9edc7b06180cdc4783058eec0b306ccc7
Fernando B Oliveira <fboliveira25@gmail.com>
2026-07-03 16:45:05 -0300

    remove list project from app

--- Codes/005-project-control-app/frontend-web/src/App.tsx
+++ Codes/005-project-control-app/frontend-web/src/App.tsx
@@ -1,12 +1,10 @@
 import AppHeader from "./components/AppHeader/AppHeader"
-import ListProjects from "./pages/projects/ListProjects"
 
 function App() {
 
   return( 
     <>
         <AppHeader title="Página principal da aplicação" />
-        <ListProjects />
     </>  
   )
 }
