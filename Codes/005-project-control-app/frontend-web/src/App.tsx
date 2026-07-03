d0b10670a8d14f4a4690d349c3ae900cce20b5df
Fernando B Oliveira <fboliveira25@gmail.com>
2026-07-03 15:34:22 -0300

    updated list project

--- Codes/005-project-control-app/frontend-web/src/App.tsx
+++ Codes/005-project-control-app/frontend-web/src/App.tsx
@@ -1,10 +1,12 @@
 import AppHeader from "./components/AppHeader/AppHeader"
+import ListProjects from "./pages/projects/ListProjects"
 
 function App() {
 
   return( 
     <>
         <AppHeader title="Página principal da aplicação" />
+        <ListProjects />
     </>  
   )
 }
